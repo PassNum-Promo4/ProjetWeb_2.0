f2880ecf132779c6e749dcde24c326d1ba552469
maariinec <cristol.marine@gmail.com>
2018-01-08 16:06:01 +0100

    GameG6

--- comp/game6/game.js
+++ comp/game6/game.js
@@ -1,17 +1,17 @@
 // author: Marine Cristol 
-var time = 50;
+var g6time = 50;
 
-var tableau = [];
-	tableau[0] = ["Qu'es que le terminal", "une interface", "un logiciel", "site web", "application"];
-	tableau[1] = ["A quoi sert le terminal?", "plus pratique", "ca change", "innovateur", "différent"];
-	tableau[2] = ["Comment faire pour se déplacer dans un répertoire en ligne de commande ?", "cd", "cat", "touch", "mv"];
-	tableau[3] = ["Comment afficher le chemin du répertoire?", "pwd", "mv", "cp", "cat"];
-	tableau[4] = ["Comment créer un dossier en paramétre ? ", "mkdir", "cp", "touch", "rm"];
-	tableau[5] = ["Comment déplacer un fichier/dossier dans un répertoire?", "mv", "cd", "mkdir", "rm"];
-	tableau[6] = ["Comment afficher dans le terminal le contenu d'un ficher ? ", "cat", "touch", "ls", "mv"];
-	tableau[7] = ["comment lister le contenu d'un répertoire ? ", "ls", "cd", "cp", "mv"];
-	tableau[8] = ["comment afficher le manuel ?", "man", "cp", "rm", "cat"];
-	tableau[9] = ["comment acquérir temporairement des droits des super-utilisateur?", "sudo", "cat", "touch", "ls"];
+var g6tableau = [];
+	g6tableau[0] = ["Qu’es que MySql?", "un logiciel", "une interface", "site web", "application"];
+	g6tableau[1] = ["Qu’es qu’une base de donnée ? ", "collection de données", "logiciel ", "application", "system"];
+	g6tableau[2] = ["Comment créer une base de données ?", "created", "show", "use", "select"];
+	g6tableau[3] = ["Comment supprimé une table ?", "drop", "show", "select", "use"];
+	g6tableau[4] = ["Comment lister une base de données ?", "show", "drop", "created", "select"];
+	g6tableau[5] = ["Comment spécifier avec quelle base de donnée on veut intéragir?", "use", "select", "describe", "alter"];
+	g6tableau[6] = ["Comment afficher la structure d’une table ? ", "describe", "alter", "truncate", "use"];
+	g6tableau[7] = ["Comment renommer une table? ", "alter", "drop", "created", "show"];
+	g6tableau[8] = ["Comment vider une table ? ", "truncate", "use", "drop", "show"];
+	g6tableau[9] = ["Comment sélectionner une table ?", "select", "truncate", "describe", "use"];
 
 function g6Checker(event){
 	if(event.target.attributes[3].nodeValue === "reponse right"){
@@ -20,93 +20,93 @@ function g6Checker(event){
 
 }
 
+	
 function g6randomAnswers(tab){
-	var tabRandom = [];
-	var intermediaire = Math.floor(Math.random() * 4)+1;
+	var g6intermediaire = Math.floor(Math.random() * 4)+1;
+	var g6tabRandom = [];
+	g6tabRandom[0] = g6intermediaire;
 
-	tabRandom[0] = intermediaire;
-
-	for(var i=1; i<4; i++){
-		while(intermediaire === tabRandom[0] || intermediaire === tabRandom[1] || intermediaire === tabRandom[2] ||intermediaire === tabRandom[3]){
-			intermediaire = Math.floor(Math.random() * 4)+1;
+	for(var g6i=1; g6i<4; g6i++){
+		while(g6intermediaire === g6tabRandom[0] || g6intermediaire === g6tabRandom[1] || g6intermediaire === g6tabRandom[2] ||g6intermediaire === g6tabRandom[3]){
+			g6intermediaire = Math.floor(Math.random() * 4)+1;
 		}
-		tabRandom[i] = intermediaire;
+		g6tabRandom[g6i] = g6intermediaire;
 	}
 
-	var htmlReponse = "";
-	for(i=0; i<4; i++){
-		if (tabRandom[i] === 1) {
-			htmlReponse += "<li>"+tableau[tab][tabRandom[i]]+"<INPUT type='radio' name='question"+ tab +"' onclick='g6Checker(event)' class='reponse right' ></li>";
+	var g6htmlReponse = "";
+	for(g6i=0; g6i<4; g6i++){
+		if (g6tabRandom[g6i] === 1) {
+			g6htmlReponse += "<li>"+g6tableau[tab][g6tabRandom[g6i]]+"<INPUT type='radio' name='question"+ tab +"' onclick='g6Checker(event)' class='reponse right' ></li>";
 		}else{
-			htmlReponse += "<li>"+tableau[tab][tabRandom[i]]+"<INPUT type='radio' name='question"+ tab +"'onclick='g6Checker(event)' class='reponse false' ></li>";
+			g6htmlReponse += "<li>"+g6tableau[tab][g6tabRandom[g6i]]+"<INPUT type='radio' name='question"+ tab +"'onclick='g6Checker(event)' class='reponse false' ></li>";
 
 		}
 	}
-	return htmlReponse;
+	return g6htmlReponse;
 
 }
 
 
 function g6randomQuestion(){
-	var tabAleatoire =[];
-	var interQuestion = Math.floor(Math.random() * 10);;
+	var g6tabAleatoire =[];
+	var g6interQuestion = Math.floor(Math.random() * 10);;
 
-		tabAleatoire[0] = interQuestion;
+		g6tabAleatoire[0] = g6interQuestion;
 
 
-	for(var i=1; i<10; i++){
-		while(interQuestion === tabAleatoire[0] || interQuestion === tabAleatoire[1] || interQuestion === tabAleatoire[2] ||interQuestion === tabAleatoire[3] ||interQuestion === tabAleatoire[4] ||interQuestion === tabAleatoire[5] ||interQuestion === tabAleatoire[6] ||interQuestion === tabAleatoire[7] ||interQuestion === tabAleatoire[8] ||interQuestion === tabAleatoire[9]){
-			interQuestion = Math.floor(Math.random() * 10);
+	for(var g6i=1; g6i<10; g6i++){
+		while(g6interQuestion === g6tabAleatoire[0] || g6interQuestion === g6tabAleatoire[1] || g6interQuestion === g6tabAleatoire[2] ||g6interQuestion === g6tabAleatoire[3] ||g6interQuestion === g6tabAleatoire[4] ||g6interQuestion === g6tabAleatoire[5] ||g6interQuestion === g6tabAleatoire[6] ||g6interQuestion === g6tabAleatoire[7] ||g6interQuestion === g6tabAleatoire[8] ||g6interQuestion === g6tabAleatoire[9]){
+			g6interQuestion = Math.floor(Math.random() * 10);
 		}
-		tabAleatoire[i] = interQuestion;
+		g6tabAleatoire[g6i] = g6interQuestion;
 	}
 
-	var htmlQuestion = "";
-	for(i=0; i<10; i++){
-		htmlQuestion += "<div class='randomQuestion' id='Q"+i+"'>"+tableau[tabAleatoire[i]][0]+"</div><ul>"+g6randomAnswers(tabAleatoire[i])+"</ul>"; 
+	var g6htmlQuestion = "";
+	for(g6i=0; g6i<10; g6i++){
+		g6htmlQuestion += "<div class='randomQuestion' id='Q"+g6i+"'>"+g6tableau[g6tabAleatoire[g6i]][0]+"</div><ul>"+g6randomAnswers(g6tabAleatoire[g6i])+"</ul>"; 
 		}
 	
-	return htmlQuestion;
+	return g6htmlQuestion;
 }
 
 
 
 function g6GenerateQuizz(){
 	document.getElementById('quizz').innerHTML= "Timer/seconde: <div id='timer'></div>Score:<div id='score'></div>"+g6randomQuestion();
-	var monInterval=window.setInterval(function(){g6Timer(monInterval)},1000);
+	var g6monInterval=window.setInterval(function(){g6Timer(g6monInterval)},1000);
 	loadNextMiniGame();
 
 }
 
 function g6Timer(monInterval){
 	
-	if (time <= 0) {
+	if (g6time <= 0) {
 	g6StopTimer(monInterval);
 	}
 	else{
-		time -= 1; 
-	document.getElementById('timer').innerHTML=time;
+		g6time -= 1; 
+	document.getElementById('timer').innerHTML=g6time;
 }	
 }
 
 
 function g6StopTimer(stop){
 	window.clearInterval(stop);
-	var tabradio = document.getElementsByClassName('reponse');
-	for(var i=0;i<tabradio.length;i++){
-		tabradio[i].disabled=true;
+	var g6tabradio = document.getElementsByClassName('reponse');
+	for(var g6i=0;g6i<g6tabradio.length;g6i++){
+		g6tabradio[g6i].disabled=true;
 	}
 	addPoints();
 }
 function addPoints(){
-	var score = 0;
-	var point = document.getElementsByClassName('reponse');
-	for(var i=0;i<point.length;i++){
-		if ( point[i].checked===true && point[i].getAttribute("class") === "reponse right"){
-			score ++;
+	var g6score = 0;
+	var g6point = document.getElementsByClassName('reponse');
+	for(var g6i=0;g6i<g6point.length;g6i++){
+		if ( g6point[g6i].checked===true && g6point[g6i].getAttribute("class") === "reponse right"){
+			g6score ++;
 		}
 	}
-	document.getElementById('score').innerHTML=score;
+	document.getElementById('score').innerHTML=g6score;
 
 }
 
