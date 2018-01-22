f9855de491d1a53e89ae70688aaebdd775a96b40
Amanda COSSU <amandamcossu@gmail.com>
2018-01-22 15:53:19 +0100

    Update game.js
    Waiting timer definition for last changes

--- comp/game13/game.js
+++ comp/game13/game.js
@@ -1,6 +1,7 @@
 /*
   Author : Amanda Medeiros
 */
+
 var g13q1 = [
     "À quoi sert le CSS ?", // Index [0] - Question
     "Styliser une page web", // Index [1] - Correct answer
@@ -16,7 +17,7 @@ var g13q2 = [
     "h1{p}"
 ];
 var g13q3 = [
-    "En CSS, comment sélectionnez-vous l’image suivante par son identifiant ? <img id = superimage src = image.png >",
+    "En CSS, comment sélectionnez-vous l’image suivante par son identifiant ? <img id = superimage src = image.png>",
     "#superimage {}",
     "superimage {}",
     "img { }",
@@ -30,7 +31,7 @@ var g13q4 = [
     "Afficher des animations"
 ];
 var g13q5 = [
-    "Quelle propriété peut être utilisé pour changer le type police, en CSS ?",
+    "Quelle propriété peut être utilisé pour changer le type police?",
     "font-family",
     "font",
     "font-color",
@@ -40,8 +41,8 @@ var g13q6 = [
     "Qu’est ce que la pseudo-classe :hover permet de changer ?",
     "L’aspect d’un élément",
     "Le téléchargement d’un élément",
-    "La balise <body>",
-    "La balise <p>"
+    "La balise &lt;body&gt;",
+    "La balise &lt;p&gt;"
 ];
 var g13q7 = [
     "Quelle est la fonction du pseudo-element :before ?",
@@ -61,8 +62,8 @@ var g13q9 = [
     "La propriété padding spécifie l’espace entre…",
     "Le bord d’un élément et son contenu",
     "Les bordes de deux éléments",
-    "Deux balises <p>",
-    "Les balises <body> et <head>"
+    "Deux balises &lt;p&gt;",
+    "Les balises &lt;body&gt; et &lt;head&gt;"
 ];
 var g13q10 = [
     "Dans un flex container, quelle est l’orientation des éléments par défaut ?",
@@ -92,7 +93,10 @@ var g13currentTabQ = [];
 
 var g13intervalId = null;
 
-var g13divAnswer = document.querySelector("#answer");
+var g13A1 = document.querySelector("#answer1");
+var g13A2 = document.querySelector("#answer2");
+var g13A3 = document.querySelector("#answer3");
+var g13A4 = document.querySelector("#answer4");
 
 var g13divQuestion = document.querySelector("#question");
 
@@ -106,25 +110,26 @@ function g13startgame() {
 
 //reset timer
 function g13reset() {
-    document.getElementById("g13chrono").innerHTML = g13counter + " secondes";
+    //document.getElementById("g13chrono").innerHTML = g13counter + " secondes";
     g13counter--;
     g13resetTimer();
 }
 
 // end of the game
+/*
 function g13endGame() {
-    clearInterval(g13intervalId);
+    //clearInterval(g13intervalId);
     document.querySelector(".display").innerHTML = "";
     g13divAnswer.innerHTML = "GAME OVER";
     g13divQuestion.innerHTML = "";
 
 }
+*/
 // function that stops the timer in the end of the game and load the next game
-function g13resetTimer(){
-    if (g13counter == -1) {
-      setTimeout(g13getRandomQA, 1500);
-      g13counter = 10;
-      
+function g13resetTimer() {
+    if (g13counter < 0) {
+        g13counter = 10;
+        setTimeout(g13getRandomQA, 1500);
     }
 }
 
@@ -162,30 +167,49 @@ function g13exists(valeur, tab) {
 
 //random question and answers without repeating it
 function g13getRandomQA() {
+    //output reinit
+    g13A1.parentElement.classList.remove("bg-danger");
+    g13A1.parentElement.classList.remove("bg-success");
+    g13A2.parentElement.classList.remove("bg-danger");
+    g13A2.parentElement.classList.remove("bg-success");
+    g13A3.parentElement.classList.remove("bg-danger");
+    g13A3.parentElement.classList.remove("bg-success");
+    g13A4.parentElement.classList.remove("bg-danger");
+    g13A4.parentElement.classList.remove("bg-success");
+
     if (g13round == 10) {
-        g13endGame();
+        //g13endGame();
         clearTimeout(g13intervalId);
         setTimeout(loadNextMiniGame, 2000);
-        
-    }else{
-    g13currentTabQ = g13RandQ(g13_AllQ); // contains the current table with question and answers
-    var g13ElemQ = document.createElement('h2');
-    g13ElemQ.textContent = g13currentTabQ[0]; // contains the text of the question
-    g13divQuestion.innerHTML = ''; // empties the div having the id = question
-    g13divQuestion.appendChild(g13ElemQ); // adds the text of the question to the div having id = question
-    g13divAnswer.innerHTML = ''; 
-    g13ansrand = g13RA(g13currentTabQ);
-    
-    for (var i = 1; i < 5; i++) { // show the answers
-        var g13ElemA = document.createElement('input');
-        g13ElemA.value = g13ansrand[i];
-        g13ElemA.type = 'button';
-        g13ElemA.style = 'width:1000px'; // needed to show the entire answer
-        g13ElemA.addEventListener('click', g13result);
-        g13divAnswer.appendChild(g13ElemA);
-       
-    }
-    g13round++;
+
+    } else {
+        g13currentTabQ = g13RandQ(g13_AllQ); // contains the current table with question and answers
+        var g13ElemQ = document.createElement('h3');
+        g13ElemQ.textContent = g13currentTabQ[0]; // contains the text of the question
+        g13divQuestion.innerHTML = ''; // empties the div having the id = question
+        g13divQuestion.appendChild(g13ElemQ); // adds the text of the question to the div having id = question
+        //g13divAnswer.innerHTML = ''; 
+        g13ansrand = g13RA(g13currentTabQ);
+
+        // for (var i = 1; i < 5; i++) { // show the answers
+        //var g13ElemA = document.createElement('input');
+        //g13ElemA.value = g13ansrand[i];
+        //g13ElemA.type = 'button';
+        //g13ElemA.style = 'width:1000px'; // needed to show the entire answer
+
+        g13A1.innerHTML = g13ansrand[1];
+        g13A2.innerHTML = g13ansrand[2];
+        g13A3.innerHTML = g13ansrand[3];
+        g13A4.innerHTML = g13ansrand[4];
+
+        g13A1.parentElement.addEventListener('click', g13result);
+        g13A2.parentElement.addEventListener('click', g13result);
+        g13A3.parentElement.addEventListener('click', g13result);
+        g13A4.parentElement.addEventListener('click', g13result);
+        //g13divAnswer.appendChild(g13ElemA);
+
+
+        g13round++;
 
     }
     g13resetTimer();
@@ -203,19 +227,41 @@ function g13RandQ(tab) {
 }
 
 // increment the score, call the next question on click
-function g13result(){
-
-    if (this.value == g13currentTabQ[1]) {
+function g13result() {
+    var g13indexFound = g13ansrand.indexOf(g13currentTabQ[1]);
+        var g13id = "answer" + g13indexFound;
+        var g13p = document.getElementById(g13id);
+        g13p.parentElement.classList.add("bg-success");
+/*
+    switch (g13indexFound) {
+        case 1:
+            g13A1.parentElement.classList.add("bg-success");
+            break;
+        case 2:
+            g13A2.parentElement.classList.add("bg-success");
+            break;
+        case 3:
+            g13A3.parentElement.classList.add("bg-success");
+            break;
+        case 4:
+            g13A4.parentElement.classList.add("bg-success");
+            break;
+    }
+*/
+    if (this.children[0].innerHTML == g13currentTabQ[1]) {
         g13score = g13score + 1;
         g13counter = 10;
-        addpoints(1);
-    }else{
+        //addpoints(1);
+    } else {
+        // afficher l'erreur
+        this.classList.add("bg-danger");
         g13score = g13score;
         g13counter = 10;
-         
     }
-    g13getRandomQA();
-    document.getElementById('score').innerHTML = "Score : " + g13score + " " + "/ 10 questions correctes";
-    g13reset();
-}
 
+    //document.getElementById('score').innerHTML = "Score : " + g13score + " " + "/ 10 questions correctes";
+    // go to next question
+    setTimeout(g13getRandomQA, 1500);
+
+    //g13reset();
+}
