5bd89546eb5d039477d36e8df96b7aa34c07d04b
AMRAOUI-Bilel <33541876+bilelamraoui@users.noreply.github.com>
2018-01-26 09:15:46 +0100

    Update game.js

--- comp/game2/game.js
+++ comp/game2/game.js
@@ -1,228 +1,194 @@
+//
+// /* Author :bilel AMRAOUI */
+//
 
-/* Author :bilel AMRAOUI */
+var g2container = document.querySelector('#g2container');
+var g2q1 = [
+  'comment rendre la règle h1 {font-style:italic;} prioritaire par rapport à toutes ?', //question
+  'h1 {font-style:italic !important; }', //bonne reponce
+  'h1 override{font-style:italic; }', //mauvaise reponce
+  'h1 no_override {font-style:italic; }', //mauvaise reponce
+  'h1#high {font-style:italic; }' //mauvaise reponce
+];
+
+var g2q2 = [
+  'Que se passe-t-il pour le texte placé entre ces balises <canvas> Texte </canvas> ?',
+  'ignoré si le navigateur ne supporte pas les balises',
+  'toujour affiché',
+  'affiché',
+  'jamais affiché '
+];
+
+var g2q3 = [
+  'Quelle nouvelle balise de section permet de regrouper un contenu tangentiel au contenu principal du document ?',
+  '<aside>',
+  '<section id="sidebar">',
+  '<sidebar>',
+  '<details>'
+];
+
+var g2q4 = [
+  'La nouvelle balise <time> permet de baliser une date structurée. Quelle serait sa syntaxe pour le 1er avril 2012 à 13h37 ?',
+  '<time datetime="2012-04-01T13:37:00Z"></time>',
+  '<time value="2012-04-0113:37"></time>',
+  '<time datetime="01/04/2012"></time>',
+  '<time date=01/04/2012></time>'
+];
+
+var g2q5 = [
+  'À partir de quelle version d Internet Explorer peut-on utiliser nativement les éléments de section HTML5 (sans hack ou script complémentaire) ?',
+  'Internet Explorer 9',
+  'Internet Explorer 8',
+  'Internet Explorer 10',
+  'Internet Explorer 7'
+];
+
+var g2q6 = [
+  'Quelle est la méthode pour associer une légende complète à une illustration ?',
+  '<figure><img src="image.jpg"></figure>',
+  '<figuresrc="image.jpg"legend>',
+  '<figure><legend>Lalégende</legend>',
+  '<figure><legend>Lalégende</legend>'
+];
+
+var g2q7 = [
+  'Comment représenter une barre de progression à 50% d avancement ?',
+  '<progress value="50" max="100">50%</progress>',
+  '<input type="progress" value="0.5">50%>',
+  '<input type="progress" value="50">',
+  '<input value="50" max="100" title="50%">'
+];
+
+var g2q8 = [
+  'Comment associer une liste de choix/suggestions à un champ d entrée texte ?',
+  '<input list="fruits"><datalist id="fruits"></datalist>',
+  '<input list="fruits">',
+  '<input datalist="fruits">',
+  '<input datalist="fruits">'
+];
+
+var g2q9 = [
+  'Quel attribut permet d afficher une image par défaut pour l élément <video> ?',
+  '<video poster="apercu.jpg">',
+  '<video preview="apercu.jpg">',
+  '<video><param name="thumbnail">',
+  '<video preview="apercu.png">'
+];
+
+var g2q10 = [
+  'Quelle balise doit permettre l inclusion de sous-titres textes dans les vidéos lues avec <video> ?',
+  '<track src="soustitres.vtt">',
+  '<subtitle source="soustitres.srt">',
+  '<captions source="soustitres.srt">',
+  '<option source="soustitres">'
+];
+
+var g2qtab = [g2q1, g2q2, g2q3, g2q4, g2q5, g2q6, g2q7, g2q8, g2q9, g2q10]; // tableau contenant chaque tableau de question/reponse
+var g2Time = 9;
 
 
-var g2Q1 = new Array('comment rendre la règle h1 {font-style:italic;} prioritaire par rapport à toutes ?','h1 {font-style: italic !important; }','h1 override{font-style: italic; }','h1 no_override {font-style: italic; }','h1#high {font-style: italic; }');
-var g2Q2 = new Array('Que se passe-t-il pour le texte placé entre ces balises <canvas> Texte </canvas> ?','il est ignoré sauf si le navigateur ne supporte pas ces balises','il est toujour affiché','il est affiché, sauf si le navigateur ne supporte pas ces balises','il n est jamais affiché ');
-var g2Q3 = new Array('Quelle nouvelle balise de section permet de regrouper un contenu tangentiel au contenu principal du document ?','<aside>','<section id="sidebar">','<sidebar>','<details>');
-var g2Q4 = new Array('La nouvelle balise <time> permet de baliser une date structurée. Quelle serait sa syntaxe pour le 1er avril 2012 à 13h37 ?','<time datetime="2012-04-01T13:37:00Z"></time>','<time value="2012-04-01 13:37"></time>','<time datetime="01/04/2012 13H37M00S"></time>','<time date=01/04/2012 13H37M00S></time>');
-var g2Q5 = new Array('À partir de quelle version d Internet Explorer peut-on utiliser nativement les éléments de section HTML5 (sans hack ou script complémentaire) ?','Internet Explorer 9','Internet Explorer 8','Internet Explorer 10','Internet Explorer 7');
-var g2Q6 = new Array('Quelle est la méthode pour associer une légende complète à une illustration ?','<figure><img src="image.jpg"><figcaption>La légende...</figcaption></figure>','<figure src="image.jpg" legend="#cap1"></figure><figcaption id="cap1">La légende...</figcaption>','<figure><legend>La légende...</legend><img src="image.jpg"></figure>','<figure><legend>La légende...</legend><img url="image.jpg"></figure>');
-var g2Q7 = new Array('Comment représenter une barre de progression à 50% d avancement ?','<progress value="50" max="100">50%</progress>','<input type="progress" value="0.5">50%</progress>','<input type="progress" value="50" max="100" title="50%" />','<input value="50" max="100" title="50%" />');
-var g2Q8 = new Array('Comment associer une liste de choix/suggestions à un champ d entrée texte ?','<input list="fruits"><datalist id="fruits"><option>Kiwi</option><option>Orange</option><option>Mangue</option></datalist>','<input list="fruits"><select><datalist id="fruits" values="Kiwi,Orange,Mangue" /></select>','<input datalist="fruits"><list id="fruits"><option value="Kiwi"><option value="Orange"><option value="Mangue"></list>',
-                     '<input datalist="fruits"><list id="fruits"><option><value="Orange"><option value="Mangue"></list>');
-var g2Q9 = new Array('Quel attribut permet d afficher une image par défaut pour l élément <video> ?','<video poster="apercu.jpg">','<video preview="apercu.jpg">','<video><param name="thumbnail" value="apercu.jpg" /></video>','<video preview="apercu.png">');
-var g2Q10 = new Array('Quelle balise doit permettre l inclusion de sous-titres textes dans les vidéos lues avec <video> ?','<track src="soustitres.vtt">','<subtitle source="soustitres.srt">','<captions source="soustitres.srt">','<option source="soustitres">');
-var g2AllQ = new Array(g2Q1,g2Q2,g2Q3,g2Q4,g2Q5,g2Q6,g2Q7,g2Q8,g2Q9,g2Q10);
-var g2Q = '';
-var g2doNotUseA = new Array('');
-var g2doNotUseQ = new Array('');
-var g2Qdiv = '';
-var g2A1div = '';
-var g2A2div = '';
-var g2A3div = '';
-var g2A4div = '';
-var g2roundDone = 0;
-var g2Timer;
-var g2TimeLeft = 90; // le timer generale du jeux et de 90 seconds
-var g2QTimer;
-var g2QTimeLeft = 9;// le timer des question et de 9 seconds
-var g2TimeRst;
-var g2rlt = 0;
-var g2CurrentQ;
-var g2iH = 1;
+setTimer(g2Time, g2resultQ); // fonction d'appel qui définit l'heure de la barre de progression anim et la fonction qui sera appelée à la fin de l'animation
 
 
-g2startGame();
+resetGameTitle("QUIZZ HTML"); // titre du quiz
 
+function g2resultQ() {
+  document.querySelector('.progress-bar').classList.add("paused"); //le timer en progress-bar
+  this.classList.add("bg__false-answer"); // mauvaise reponce backgroundColor = red
+  document.querySelector('#g2true').classList.remove("bg__false-answer");
+  document.querySelector('#g2true').classList.add("bg__true-answer"); // bonne reponce backgroundColor = green
+  document.querySelector('#g2true').removeEventListener("click", g2score);
+  setTimeout(function() {
+    g2randomQ()
+  }, 2000); // au moment du click 2 seconde d'interval
 
-function g2QTimerdown() { //cela et la fonction pour le timer par questions
-  g2QTimer = window.setTimeout('g2UpdateQTimer()', 1000);
-}
-
-
-function g2UpdateQTimer() {
-  document.querySelector('#g2QTimer').textContent = 'Temps Manche : ' + g2QTimeLeft;
-  if (g2QTimeLeft <= 0) {
-    g2CurrentQ.classList.add('text-warning');
-    document.querySelector('#g2Result').appendChild(g2CurrentQ);
-    g2rst();
-  } else {
-    g2QTimeLeft--;
-    g2QTimerdown();
+  var g2disabled = [];
+  for (var i = 0; i < 4; i++) {
+    g2disabled.push(document.querySelectorAll('.g2myAnswers')[i]);
   }
-}
-
-
-function g2startGame() { //cela et la fonction qu'on appelle qu'en on appuit sur start
-  g2resetGame();
-  g2getRandom();
-  g2QTimerdown();
-  g2roundDone = 0;
-}
-
-
-function g2resetGame() {
-  g2doNotUseA = [];
-  document.querySelector('#g2Q').innerHTML = '';
-  document.querySelector('#g2A1-4').innerHTML = '';
-}
-
-
-function g2getRandom() { //fonction qui permet de choisir aleatoiremen la question et la reponse
-
-  while (g2roundDone < 10) {
-    var i = Math.floor(Math.random() * g2AllQ.length);
-    if (g2doNotUseQ.indexOf(i) === -1) {
-      g2doNotUseQ[g2roundDone] = i;
-      g2Q = g2AllQ[i];
-      g2createArea();
-      g2CurrentQ = document.createElement('span');
-      g2CurrentQ.classList.add('mr-1');
-      g2CurrentQ.textContent = ' Q' + (g2iH++);
-      return g2Q;
-    }
+  for (var i = 0; i < g2disabled.length; i++) {
+    g2disabled[i].removeEventListener("click", g2resultQ);
+    g2disabled[i].classList.remove("btn_hover");
   }
-}
-
-
-function g2createArea() { //Creation des div pour les questions reponces
-  g2Qdiv = document.createElement('div');
-  g2Qdiv.id = 'g2Q1'
-  g2Qdiv.classList.add('col-lg-8', 'col-md-8', 'col-sm-8', 'border', 'border-dark', 'rounded', 'p-3', 'm-3');
-  g2Qdiv.textContent = g2Q[0];
-
-  g2A1div = document.createElement('button');
-  g2A1div.id = 'g2A1';
-  g2A1div.classList.add('col-lg-4', 'col-md-12', 'col-sm-12', 'border', 'border-success', 'rounded', 'p-2', 'm-1');
-  g2A1div.textContent = g2Q[1];
-
-  g2A2div = document.createElement('button');
-  g2A2div.id = 'g2A2';
-  g2A2div.classList.add('col-lg-4', 'col-md-12', 'col-sm-12', 'border', 'border-success', 'rounded', 'p-2', 'm-1');
-  g2A2div.textContent = g2Q[2];
-
-  g2A3div = document.createElement('button');
-  g2A3div.id = 'g2A3';
-  g2A3div.classList.add('col-lg-4', 'col-md-12', 'col-sm-12', 'border', 'border-success', 'rounded', 'p-2', 'm-1');
-  g2A3div.textContent = g2Q[3];
-
-  g2A4div = document.createElement('button');
-  g2A4div.id = 'g2A4';
-  g2A4div.classList.add('col-lg-4', 'col-md-12', 'col-sm-12', 'border', 'border-success', 'rounded', 'p-2', 'm-1');
-  g2A4div.textContent = g2Q[4];
-
-  g2pushQ();
-}
-
-
-
-function g2pushQ() {
-document.querySelector('#g2Q').appendChild((g2Qdiv));
-  var j = 0;
-  while (j < 4) {
-    var i = Math.floor(Math.random() * 4) + 1;
-    if (g2doNotUseA.indexOf(i) === -1) {
-      g2doNotUseA[j] = i;
-      switch (i) {
-        case 1:
-          document.querySelector('#g2A1-4').appendChild((g2A1div));
-          break;
-        case 2:
-          document.querySelector('#g2A1-4').appendChild((g2A2div));
-          break;
-        case 3:
-          document.querySelector('#g2A1-4').appendChild((g2A3div));
-          break;
-        case 4:
-          document.querySelector('#g2A1-4').appendChild((g2A4div));
-          break;
-      }
-      j++
-    }
-  }
-  g2ForClick();
-}
-
-
-function g2ForClick() {
-  document.querySelector('#g2A1').addEventListener('click', g2trueAnswer);
-  document.querySelector('#g2A2').addEventListener('click', g2falseAnswer);
-  document.querySelector('#g2A3').addEventListener('click', g2falseAnswer);
-  document.querySelector('#g2A4').addEventListener('click', g2falseAnswer);
-}
-
-
-function g2falseAnswer() {
-  g2CurrentQ.classList.add('text-danger');
-  document.querySelector('#g2Result').appendChild(g2CurrentQ);
-  g2rst();
 
 }
 
 
-function g2trueAnswer() {
-  g2CurrentQ.classList.add('text-success');
-  document.querySelector('#g2Result').appendChild(g2CurrentQ);
-  g2rst();
+function g2score() { // a chaque bonne rebonce le score augmente de 1 point
   addPoints(1);
-  g2score();
-
 }
 
-function g2rst() {
-  document.querySelector('#g2A1').removeEventListener('click', g2trueAnswer);
-  document.querySelector('#g2A2').removeEventListener('click', g2falseAnswer);
-  document.querySelector('#g2A3').removeEventListener('click', g2falseAnswer);
-  document.querySelector('#g2A4').removeEventListener('click', g2falseAnswer);
-  clearTimeout(g2QTimer);
-  g2A1div.classList.add('col-lg-4', 'col-md-12', 'col-sm-12', 'border', 'border-success', 'rounded', 'p-2', 'm-1','btn','btn-success','bg-success');
-  g2A2div.classList.add('col-lg-4', 'col-md-12', 'col-sm-12', 'border', 'border-danger', 'rounded', 'p-2', 'm-1','btn','btn-danger','bg-danger');
-  g2A3div.classList.add('col-lg-4', 'col-md-12', 'col-sm-12', 'border', 'border-danger', 'rounded', 'p-2', 'm-1','btn','btn-danger','bg-danger');
-  g2A4div.classList.add('col-lg-4', 'col-md-12', 'col-sm-12', 'border', 'border-danger', 'rounded', 'p-2', 'm-1','btn','btn-danger','bg-danger');
-  g2TimeRst = window.setTimeout('g2nextQ()',1000);
 
-
-
-
+function g2gameOver() { // c'est la fonction qui et apeller a la fin du jeux
+  //clearTimeout(g18uptadeTime);// stop timer
+  document.querySelector('#g2R1').innerHTML = ""; // netoyage de la page
+  document.querySelector('#g2R2').innerHTML = "";
+  document.querySelector('#g2R3').innerHTML = "";
+  document.querySelector('#g2R4').innerHTML = "";
+  document.querySelector('#g2QetT').style.display = "none";
+  document.querySelector('#g2form__submit').value = "Next Game";
+  document.querySelector('#g2form__submit').disabled = false;
+  document.querySelector('#g2form__submit').style.display = "block";
+  document.querySelector('#g2form__submit').addEventListener("click", loadNextMiniGame); // au moment du click sur le bouton next games on appel la fonction loadNextMiniGame
 }
 
-function g2nextQ() {
+function g2randomQ() { // function called to generate new Q? and rep
 
-  if (g2roundDone != 9) {
-    g2roundDone++;
-    g2resetGame();
-    g2getRandom();
-    window.clearTimeout(g2QTimer);
-    g2QTimeLeft = 9;
-    g2UpdateQTimer();
-
-
+  document.querySelector('.progress-bar').classList.remove("paused"); // restart de la progressbar
+  var g2ProgressBar = document.querySelector('.progress-bar');
+  document.querySelector('.progress').innerHTML = ""; // netoyage de la progressbar
+  var g2progress = document.querySelector('.progress');
+  g2progress.appendChild(g2ProgressBar);
+  if (g2qtab.length < 1) {
+    g2gameOver();
   } else {
-    g2QTimeLeft = 0;
-    g2TimeLeft = 0;
-    window.clearTimeout(g2QTimer);
-    window.clearTimeout(g2Timer);
-    g2resetGame();
-    g2nextG();
+
+
+    document.querySelector('#g2R1').innerHTML = "";
+    document.querySelector('#g2R2').innerHTML = "";
+    document.querySelector('#g2R3').innerHTML = "";
+    document.querySelector('#g2R4').innerHTML = "";
+    document.querySelector('#g2form__submit').disabled = true;
+    document.querySelector('#g2form__submit').style.display = "none";
+    var g2rdQ = Math.floor(Math.random() * g2qtab.length);
+    var g2Q = document.querySelector(".g2Qzone");
+    var g2Qcontainer = document.querySelector('#g2Qcontainer');
+    g2Q.textContent = g2qtab[g2rdQ][0];
+    g2Qcontainer.appendChild(g2Q);
+    g2qtab[g2rdQ].shift();
+    var g2R1 = document.querySelector('#g2R1');
+    var g2R2 = document.querySelector('#g2R2');
+    var g2R3 = document.querySelector('#g2R3');
+    var g2R4 = document.querySelector('#g2R4');
+
+
+    var g2tabRep = []; //creation d'un button
+
+    for (var g2i = 0; g2i < g2qtab[g2rdQ].length; g2i++) {
+      var g2reponse = document.createElement("button");
+      var g2repText = document.createElement("p");
+      g2repText.textContent = g2qtab[g2rdQ][g2i];
+      g2reponse.appendChild(g2repText);
+      if (g2i == 0) {
+        g2reponse.id = "g2true";
+        g2reponse.addEventListener('click', g2score);
+      } else {
+        g2reponse.className = "g2false";
+      }
+      g2reponse.classList.add("btn_hover", "bg-color__body", "p-4", "btn", "btn-lg", "col-12", "mt-2", "text-dark", "round", "g2myAnswers") //style of the buttons
+      g2reponse.addEventListener('click', g2resultQ);
+      g2tabRep.push(g2reponse);
+    }
+    g2tabRep.sort(function(a, b) {
+      return 0.5 - Math.random()
+    });
+    g2R1.appendChild(g2tabRep[0]);
+    g2R2.appendChild(g2tabRep[1]);
+    g2R3.appendChild(g2tabRep[2]);
+    g2R4.appendChild(g2tabRep[3]);
+    g2qtab.splice(g2rdQ, 1);
   }
 }
 
 
-
-
-function g2score() {
-  g2rlt = g2rlt + 1;
-  document.querySelector('#g2scoreFinal').textContent = 'score ' + g2rlt + '/10';
-}
-
-
-function g2nextG() {
-  document.querySelector('#g2nextGame').textContent = 'Next Game';
-  document.querySelector('#g2nextGame').addEventListener('click', g2nGame);
-}
-
-
-function g2nGame() {
-  loadNextMiniGame();
-}
+g2randomQ(); // appel de la Q?
